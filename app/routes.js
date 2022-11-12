381e4f4ff3503d27195a1ed3233a0944fe29840c
Sydnei Davis <sydnei.dev@gmail.com>
2022-11-11 21:13:46 -0500

    routes added. animation added

--- app/routes.js
+++ app/routes.js
@@ -1,126 +1,128 @@
-module.exports = function(app, passport, db) {
+module.exports = function (app, passport, db) {
 
-// normal routes ===============================================================
+  // normal routes ===============================================================
 
-    // show the landing page (will also have our login links)
-    app.get('/', function(req, res) {
-        res.render('index.ejs');
-    });
-
-    // PROFILE SECTION =========================
-    app.get('/profile', isLoggedIn, function(req, res) {
-        db.collection('groceries').find().toArray((err, result) => {
-          if (err) return console.log(err)
-          res.render('profile.ejs', {
-            user : req.user,
-            groceries: req.groceries
-          })
-        })
-    });
-
-    // GROCERY LIST CREATION SECTION ========================= 
-    app.get('/grocery_list', function(req, res) {
-      res.render('grocery-list.ejs');
+  // show the landing page (will also have our login links)
+  app.get('/', function (req, res) {
+    res.render('index.ejs');
   });
 
-    // MANAGE FRIDGE SECTION ========================= 
-    app.get('/manage_fridge', function(req, res) {
-      res.render('manage-fridge.ejs');
+  // PROFILE SECTION =========================
+  app.get('/profile', isLoggedIn, function (req, res) {
+    res.render('profile.ejs', {
+      user: req.user.local
+    })
   });
-    
 
-    // LOGOUT ==============================
-    app.get('/logout', function(req, res) {
-        req.logout();
-        res.redirect('/');
-    });
-
-// message board routes ===============================================================
-
-    app.post('/groceries', (req, res) => {
-      db.collection('groceries').save({groceries: req.body.groceries}, (err, result) => {
+  // GROCERY LIST CREATION SECTION ========================= 
+  app.get('/grocery_list', isLoggedIn, function (req, res) {
+      db.collection('groceries').find({userID: req.user._id}).toArray((err, groceries) => {
+        console.log(groceries)
         if (err) return console.log(err)
-        console.log('saved to database')
-        res.redirect('/grocery_list')
+          res.render('grocery-list.ejs', {
+            groceries
       })
     })
+  });
 
-    app.put('/groceries', (req, res) => {
-      db.collection('groceries')
-      .findOneAndUpdate({groceries: req.body.groceries}, {
+  // MANAGE FRIDGE SECTION ========================= 
+  app.get('/manage_fridge', function (req, res) {
+    res.render('manage-fridge.ejs');
+  });
+
+
+  // LOGOUT ==============================
+  app.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/');
+  });
+
+  // message board routes ===============================================================
+
+  app.post('/groceries', (req, res) => {
+    db.collection('groceries').save({ groceries: req.body.groceries, userID: req.user._id }, (err, result) => {
+      if (err) return console.log(err)
+      console.log('saved to database')
+      res.redirect('/grocery_list')
+    })
+  })
+
+  app.put('/groceries', (req, res) => {
+    db.collection('groceries')
+      .findOneAndUpdate({ groceries: req.body.groceries }, {
         $set: {
           groceries: req.body.groceries
         }
       }, {
-        sort: {_id: -1},
+        sort: { _id: -1 },
         upsert: true
       }, (err, result) => {
         if (err) return res.send(err)
         res.send(result)
       })
+  })
+
+  app.delete('/groceries', (req, res) => {
+    db.collection('groceries').findOneAndDelete({ groceries: req.body.groceries, userID: req.user._id  }, (err, result) => {
+      if (err) return res.send(500, err)
+      res.send('Message deleted!')
     })
+  })
 
-    app.delete('/groceries', (req, res) => {
-      db.collection('groceries').findOneAndDelete({groceries: req.body.groceries}, (err, result) => {
-        if (err) return res.send(500, err)
-        res.send('Message deleted!')
-      })
-    })
+  // =============================================================================
+  // AUTHENTICATE (FIRST LOGIN) ==================================================
+  // =============================================================================
 
-// =============================================================================
-// AUTHENTICATE (FIRST LOGIN) ==================================================
-// =============================================================================
+  // locally --------------------------------
+  // LOGIN ===============================
+  // show the login form
+  app.get('/login', function (req, res) {
+    res.render('login.ejs', { message: req.flash('loginMessage') });
+  });
 
-    // locally --------------------------------
-        // LOGIN ===============================
-        // show the login form
-        app.get('/login', function(req, res) {
-            res.render('login.ejs', { message: req.flash('loginMessage') });
-        });
+  // process the login form
+  app.post('/login', passport.authenticate('local-login', {
+    successRedirect: '/profile', // redirect to the secure profile section
+    failureRedirect: '/login', // redirect back to the signup page if there is an error
+    failureFlash: true // allow flash messages
+  }));
 
-        // process the login form
-        app.post('/login', passport.authenticate('local-login', {
-            successRedirect : '/profile', // redirect to the secure profile section
-            failureRedirect : '/login', // redirect back to the signup page if there is an error
-            failureFlash : true // allow flash messages
-        }));
+  // SIGNUP =================================
+  // show the signup form
+  app.get('/signup', function (req, res) {
+    res.render('signup.ejs', { message: req.flash('signupMessage') });
+  });
 
-        // SIGNUP =================================
-        // show the signup form
-        app.get('/signup', function(req, res) {
-            res.render('signup.ejs', { message: req.flash('signupMessage') });
-        });
+  // process the signup form
+  app.post('/signup', passport.authenticate('local-signup', {
+    successRedirect: '/profile', // redirect to the secure profile section
+    failureRedirect: '/signup', // redirect back to the signup page if there is an error
+    failureFlash: true // allow flash messages
+  }));
 
-        // process the signup form
-        app.post('/signup', passport.authenticate('local-signup', {
-            successRedirect : '/profile', // redirect to the secure profile section
-            failureRedirect : '/signup', // redirect back to the signup page if there is an error
-            failureFlash : true // allow flash messages
-        }));
+  // =============================================================================
+  // UNLINK ACCOUNTS =============================================================
+  // =============================================================================
+  // used to unlink accounts. for social accounts, just remove the token
+  // for local account, remove email and password
+  // user account will stay active in case they want to reconnect in the future
 
-// =============================================================================
-// UNLINK ACCOUNTS =============================================================
-// =============================================================================
-// used to unlink accounts. for social accounts, just remove the token
-// for local account, remove email and password
-// user account will stay active in case they want to reconnect in the future
-
-    // local -----------------------------------
-    app.get('/unlink/local', isLoggedIn, function(req, res) {
-        var user            = req.user;
-        user.local.email    = undefined;
-        user.local.password = undefined;
-        user.save(function(err) {
-            res.redirect('/profile');
-        });
+  // local -----------------------------------
+  app.get('/unlink/local', isLoggedIn, function (req, res) {
+    var user = req.user;
+    user.local.email = undefined;
+    user.local.password = undefined;
+    user.save(function (err) {
+      res.redirect('/profile');
     });
+  });
 
 };
 
 // route middleware to ensure user is logged in
 function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated())
-        return next();
+  if (req.isAuthenticated())
+    return next();
 
-    res.redirect('/');
+  res.redirect('/');
 }
